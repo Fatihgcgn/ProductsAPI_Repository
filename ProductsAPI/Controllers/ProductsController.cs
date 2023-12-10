aab0f0deb04139260e3a0a7de1c26e0af4e9fe92
Fatih İdris Geçgin <fatih-gecgin@hotmail.com>
2023-12-10 21:27:37 +0300

    EntityFramework bölümüne kadar kodlandı
    EntityFramework bölümüne kadar kodlandı

--- ProductsAPI/Controllers/ProductsController.cs
+++ ProductsAPI/Controllers/ProductsController.cs
@@ -1,9 +1,79 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+using ProductsAPI.Models;
 
 namespace ProductsAPI.Controllers
 {
-    public class ProductsController:ControllerBase
+    //localhost:5000/api/products
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
     {
+        private static List<Product>? _products;
 
+        public ProductsController()
+        {
+            _products = new List<Product>
+            {
+                new()
+                {
+                    ProductId = 1,
+                    ProductName = "IPhone 14",
+                    Price = 60000,
+                    IsActive = true
+                },
+                new()
+                {
+                    ProductId = 2,
+                    ProductName = "IPhone 15",
+                    Price = 70000,
+                    IsActive = true
+                },
+                new()
+                {
+                    ProductId = 3,
+                    ProductName = "IPhone 16",
+                    Price = 80000,
+                    IsActive = true
+                },
+                new()
+                {
+                    ProductId = 4,
+                    ProductName = "IPhone 17",
+                    Price = 90000,
+                    IsActive = true
+                }
+            };
+        }
+
+        //localhost:5000/api/products => GET (tüm productlar için)
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            if (_products == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_products);
+        }
+
+        //localhost:5000/api/products/1 => GET (id ile belirtilen product için)
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var p = _products?.FirstOrDefault(i => i.ProductId == id);
+            if (p == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(p);
+        }
     }
 }
