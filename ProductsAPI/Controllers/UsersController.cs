1e1d064222453eddd4ae4a1d091ffe0b52d3086b
Fatih İdris Geçgin <fatih-gecgin@hotmail.com>
2024-02-01 17:51:28 +0300

    Json Web Token - JWT
    Json Web Token - JWT

--- ProductsAPI/Controllers/UsersController.cs
+++ ProductsAPI/Controllers/UsersController.cs
@@ -1,6 +1,9 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.IdentityModel.Tokens;
 using ProductsAPI.DTO;
 using ProductsAPI.Models;
 
@@ -8,25 +11,28 @@ namespace ProductsAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class UserController : ControllerBase
+    public class UsersController: ControllerBase
     {
-        private UserManager<AppUser> _userManager;
-        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _singInManager;
+        private readonly IConfiguration _configuration;
 
-        public UserController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        public UsersController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, IConfiguration configuration)
         {
             _userManager = userManager;
-            _signInManager = signInManager;
+            _singInManager = signInManager;
+            _configuration = configuration;
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> CreateUser(UserDTO model)
         {
-            if (ModelState.IsValid)
+            if(!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
-            var user = new AppUser
+            
+            var user = new AppUser 
             {
                 FullName = model.FullName,
                 UserName = model.UserName,
@@ -36,32 +42,53 @@ namespace ProductsAPI.Controllers
 
             var result = await _userManager.CreateAsync(user, model.Password);
 
-            if (result.Succeeded)
+            if(result.Succeeded)
             {
                 return StatusCode(201);
             }
             return
-            BadRequest(ModelState);
+            BadRequest(result.Errors);
         }
 
+        [HttpPost("login")]
         public async Task<IActionResult> Login(LoginDTO model)
         {
             var user = await _userManager.FindByEmailAsync(model.Email);
 
             if(user == null)
             {
-                return BadRequest(new {message = "email hatali"});
+                return BadRequest(new { message = "email hatalı" });
             }
 
-            var result = await _signInManager.CheckPasswordSignInAsync(user,model.Password,false);
+            var result = await _singInManager.CheckPasswordSignInAsync(user, model.Password,false);
 
             if(result.Succeeded)
             {
                 return Ok(
-                    new { token = "token"});
+                    new { token = GenerateJWT(user) }
+                );
             }
-
             return Unauthorized();
         }
+
+        private object GenerateJWT(AppUser user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration.GetSection("AppSettings:Secret").Value ?? "");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(
+                    new Claim[] {
+                        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                        new Claim(ClaimTypes.Name, user.UserName ?? ""),
+                    }
+                ),
+                Expires = DateTime.UtcNow.AddDays(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
     }
+
 }
